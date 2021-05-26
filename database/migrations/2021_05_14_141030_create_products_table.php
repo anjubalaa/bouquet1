fb5f0b18ae31860659b932f1b97830ebb23b4cda
anjubalaa <anjubalaa106@gmail.com>
2021-05-26 15:55:11 +0530

    integer

--- database/migrations/2021_05_14_141030_create_products_table.php
+++ database/migrations/2021_05_14_141030_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('price');
+            $table->integer('price');
             $table->string('category');
             $table->string('description');
             $table->string('gallery');
